e3541eddaf062a0f15587f29202cde04a78f64d3
Imad_github <imad.akeab.2017@gmail.com>
2019-08-26 11:47:31 +0200

    added array new massage

--- src/main/java/se/lexicon ulf/App.java
+++ src/main/java/se/lexicon ulf/App.java
@@ -1,14 +1,29 @@
-package se.lexicon ulf;
+package se.lexicon;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello GitHub" );
+public class App {
+    public static void main(String[] args) {
+        // String[] cars={"Volvo","Saab 900,"Audi 54", "Tesla Model 3"};
+        String[] cars = new String[4];
+
+        cars[0] = "Volvo 740";
+        cars[1] = "Saab 900";
+        cars[2] = "Audi 54";
+        cars[3] = "Tesla Model 3";
+
+        System.out.println("Hello GitHub");
         System.out.println("Here is my first update to my repo");
+        System.out.println("------------Cars List---------------------");
+/*
+        for (int i=0;i<cars.length;i++){
+            System.out.println(cars[i]);
     }
+    */
+for(String aCar:cars) {
+    System.out.println(aCar);
 }
+    }//End Main
+}// End of App
